c808d6a66e729eeceab25a960fc068cc835263cc
Martiyn <etigomartin4o0o@gmail.com>
2021-07-08 09:53:23 +0300

    added notes for NPM

---
+++ npm.js
@@ -0,0 +1,42 @@
+// NPM or Node Package Manager is a part of node that is essential to programming
+// With NPM we are able to install or uninstall and update the different packages or dependencies in our application
+// Using some pretty straightforward commands IN THE CONSOLE we can use NPM
+// Things that we can do with NPM include installing libraries, frameworks and other things to the application
+// NOTE it is impossible to use NPM unless you type the command IN THE CONSOLE
+// That being said let's say we want to add bootstrap to our app
+// It would be done like so:
+
+// IN THE CONSOLE: npm install bootstrap
+
+// Now with this command NPM will begin installing bootstrap to your app
+// Once it is done you can check which version of bootstrap you have in the "package.json" file under dependencies
+// As you can imagine NPM is lifechanging when creating an app as it gives you complete freedom with how you can go about it
+
+// Now there are those cases where you want to remove a package or library
+// NPM once again has this covered with another very simple command
+// Again it must be typed in the console or it will simply not work 
+// Let's say we want to remove bootstrap 
+// We would go about it like so:
+
+// IN THE CONSOLE: npm uninstall bootstrap
+
+// As you can see it is pretty straightforward and easy to work with NPM
+// It is even possible to install a github repository using npm although it's not recommended
+// Anyhow if you want to do it it would be like so:
+
+// IN THE CONSOLE: npm install (repository link)
+
+// And it is all set
+
+// Something very interesting to note is that with NPM you can bootstrap an app using a framework like React.js
+// It is extremely useful as you can begin working on your React app right away
+// It is done like so:
+
+// IN THE CONSOLE: npx create-react-app my-app
+
+// Notice how this time we use NPX instead of NPM and then we follow up with create-react-app
+// This is the command we use to initialize the creation of the react app we want
+// The "my-app" part is the name of the application
+// The name can be set to anything we want but for the sake of these notes we named it something simple
+// This is extremely benefitial as it pretty much creates for you a complete React application in which you can insert your own code
+// Having this tool at your disposal makes the use of a front-end library like React much more convenient
